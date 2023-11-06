87ce0d3e87da80e998db8ad5695d6a3383976a43
TONYFUERTE <juanantonio.majada@gmail.com>
2023-11-06 16:32:39 +0000

    POM1

---
+++ python-basics-main/name-main-python/cuso SELENIUM/POM/test.py
@@ -0,0 +1,29 @@
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
+
+class prueba_pon(unittest.TestCase):
+
+    def test_wikipedia_articulo_bueno(self):
+
+        driver = webdriver.Chrome();
+        driver.implicitly_wait(10);
+
+        driver.get("https://www.google.com");
+        driver.find_element(By.ID,"L2AGLb").click();
+        # aceptarCookies = driver.find_element(By.,"Aceptar todo")
+        # print(aceptarCookies);
+        search_bar = driver.find_element(By.NAME,"q");
+        search_bar.send_keys("wikipedia");
+        search_bar.send_keys(Keys.ENTER);
+
+        assert "wikipedia - Buscar con Google" in driver.title
+
+        
+        time.sleep(100);
+
+
+if __name__ == "__main__":
+    unittest.main()
--- python-basics-main/name-main-python/cuso SELENIUM/shadowDOM.py
+++ python-basics-main/name-main-python/cuso SELENIUM/shadowDOM.py
@@ -1,12 +1,32 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
 
 
 driver = webdriver.Chrome();
 driver.get("http://localhost:3000");
 
+wait = WebDriverWait(driver, 10, 0.2);
+cuadroMensaje = (By.ID, "resultado");
+
+cuadroMensaje_espera = wait.until(EC.presence_of_element_located(cuadroMensaje));
+
 driver.find_element(By.LINK_TEXT,"ShadowDOM").click();
+
+# driver.find_element(By.ID,"boton2"); #No puede acceder al boton del shadow
+
+"""Debemos acceder al shadow"""
+# time.sleep(2);
+nodoShadowhost = driver.find_element(By.ID, "nodoShadowHost");
+
+"""El shadow-root debe estar en estado open para poder acceder, si está en modo 
+closed no se podrá acceder."""
+nodoShadowRoot = driver.execute_script("return arguments[0].shadowRoot", nodoShadowhost);
+
+print(nodoShadowRoot.find_element(By.ID, "boton2"));  #Ahora ya podemos accedr al botón del shadowDOM
+nodoShadowRoot.find_element(By.ID, "boton2").click();
 
 time.sleep(5);
 
