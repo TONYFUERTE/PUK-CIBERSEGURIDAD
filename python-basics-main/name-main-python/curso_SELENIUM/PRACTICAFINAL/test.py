5ef3fb1d1e21d8fad4f2fa8f8479bcdf8a344ec2
TONYFUERTE <juanantonio.majada@gmail.com>
2023-12-15 16:40:13 +0000

    secuencial PROYECTO FINAL

--- python-basics-main/name-main-python/curso_SELENIUM/PRACTICAFINAL/test.py
+++ python-basics-main/name-main-python/curso_SELENIUM/PRACTICAFINAL/test.py
@@ -1,5 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import StaleElementReferenceException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
@@ -11,31 +14,44 @@ drive.implicitly_wait(20);
 drive.find_element(By.ID, "machinesButtonSearch").send_keys("Medium");
 drive.find_element(By.XPATH, "//div[@id='machinesButtonFilter']/following-sibling::button").click();
 
-# time.sleep(5);
+headers = drive.find_elements(By.XPATH, "//header");
+parrafos = drive.find_elements(By.XPATH, "//header/descendant::p");
+contador=0;
+contadorHeaders=0;
+items=[];
+contadorWindows=0;
+wait = WebDriverWait(drive, 10, (StaleElementReferenceException));
 
-sistOpes = drive.find_elements(By.XPATH, "//header/descendant::p[3]");
-contador = 1;
-for sistOpe in sistOpes:
-    sistOpeValor = sistOpe.text;
-    print(sistOpeValor);
-    if (sistOpeValor == 'Windows'):
-        # time.sleep(2);
-        print('entra');
-        # drive.find_element(By.XPATH, f"//header[{contador}]/descendant::div[7]").click();
-        # header.find_element(B)
-    contador = contador + 1;
-    if (contador > 26):
-        break
+while contador < len(parrafos):
     
+    valorParrafo = parrafos[contador].text
+    
+    if (valorParrafo == 'Linux' or valorParrafo == 'Windows' and contadorHeaders < len(headers)):
 
-sistOpes = drive.find_elements(By.XPATH, "//header/descendant::p[4]");
-contador = 1;
-for sistOpe in sistOpes:
+        listas = [];
+        if (valorParrafo == 'Windows' and contadorWindows < 3):
+            contadorWindows = contadorWindows+1;
+            
+            wait.until(EC.element_to_be_clickable((By.XPATH, "//header[{}]/descendant::div[8]".format(contadorHeaders+1)))).click();
+            # wait.until(EC.presence_of_element_located((By.XPATH, "//header[10]/descendant::li")));
+            listas = drive.find_elements(By.XPATH, "//header[{}]/descendant::li".format(contadorHeaders+1));
+            
+            nombreSistema = drive.find_element(By.XPATH, "//header[{}]/descendant::p[1]".format(contadorHeaders+1));
+            contadorItems=0;
+            items.append("");
+            items.append(f"{nombreSistema.text}: Sistema Operativo WINDOWS,  SKILLS: ");
+            while contadorItems < len(listas):
+
+                items.append(listas[contadorItems].text);
+                contadorItems = contadorItems + 1;
+                
+            contadorItems = contadorItems + 1;
+            
+        contadorHeaders = contadorHeaders + 1;
+        # print(contadorHeaders);
     contador = contador + 1;
-    if (contador > 26):
-        print(sistOpe.text);
 
-
-
+for item in items:
+    print(item);
 
 time.sleep(100);
